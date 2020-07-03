26405d36c6b33f80ce81b07926c8586fecbc65bb
Mahir Numayeer Islam <mahir.numayeer@gmail.com>
2020-07-03 11:14:04 +0800

    Finished the category activity(activity_main)

--- app/src/main/java/com/example/sectionsixudemyproject/controllers/ProductActivity.kt
+++ app/src/main/java/com/example/sectionsixudemyproject/controllers/ProductActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sectionsixudemyproject.R
 
-class ProductActivity : AppCompatActivity() {
+class ProductActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
